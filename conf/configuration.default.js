1eb42c0b62d58048dc2ee9f8104234cb04b84f49
Marcos Bjoerkelund <marcosbjorkelund@gmail.com>
2016-05-14 17:23:46 +0200

    Update default configuration file

--- conf/configuration.default.js
+++ conf/configuration.default.js
@@ -2,7 +2,12 @@
 const config = {
   // Path where the API will be accessed from the browser
   base: '/',
+  // Protocol and host to be shown when response contains URLs
+  protocol: 'http',
+  host: '127.0.0.1',
   // Port which the API server will run on
-  port: 3000
+  port: 3000,
+  // Different modes: 'production', 'development'
+  env: 'development'
 };
 module.exports = config;
