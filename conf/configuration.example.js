c2ee77ce4af2d970723bcf26bed0514639b59b7e
Marcos Bjoerkelund <marcosbjorkelund@gmail.com>
2016-07-17 13:03:06 +0200

    Prepare for microservice architecture

--- conf/configuration.example.js
+++ conf/configuration.example.js
@@ -3,14 +3,14 @@ const config = {
   // Path where the API will be accessed from the browser
   base: '/',
   // Port which the API server will run on
-  port: 3000,
+  port: 80,
   // Different modes: 'production', 'development'
   env: 'development',
   // Front-end settings, for obtaining URLs
   frontend: {
     protocol: 'http',
     host: '127.0.0.1',
-    port: 3000
+    port: 50080
   },
   // MongoDB connection string
   mongoDbUri: 'mongodb://api:api@mongodb:27017/api',
