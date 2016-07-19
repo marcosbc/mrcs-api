b0f0a31b62a0566c5af94c248fba892b5f53bdb7
Marcos Bjoerkelund <marcosbjorkelund@gmail.com>
2016-07-19 20:27:22 +0200

    Revert to wait-for-mongo until the issue with wait-for-mongoose is fixed

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@
 var path = require('path');
 var expressValidator = require('express-validator');
 var mongoose = require('mongoose');
-var waitForMongoose = require('wait-for-mongoose');
+var waitForMongo = require('wait-for-mongo');
 var microservice = require('microservice-skeleton');
 var apiUtils = require('./lib')();
 var config;
@@ -24,7 +24,7 @@ microservice.registerHook('express', (expressInstance) => {
     customValidators: apiUtils.validators.customValidators,
     customSanitizers: apiUtils.validators.customSanitizers
   }));
-  waitForMongoose(config.mongoDbUri, {
+  waitForMongo(config.mongoDbUri, {
     timeout: config.mongoDbTimeout
   }, (err) => {
     if (err) {
